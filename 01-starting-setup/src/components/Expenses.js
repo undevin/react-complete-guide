ea210ebefee1531e141c74141ff003f63cfa5b55
undevin <30535541+undevin@users.noreply.github.com>
2021-09-22 23:08:50 -0500

    Initial commit of nights work

---
+++ 01-starting-setup/src/components/Expenses.js
@@ -0,0 +1,18 @@
+import './Expenses.css';
+import ExpenseItem from './ExpenseItem';
+import Card from './Card';
+
+function Expenses(props) {
+  const expenses = props.items;
+
+  return (
+    <Card className='expenses'>
+      <ExpenseItem {...expenses[0]} />
+      <ExpenseItem {...expenses[1]} />
+      <ExpenseItem {...expenses[2]} />
+      <ExpenseItem {...expenses[3]} />
+    </Card>
+  );
+}
+
+export default Expenses;
