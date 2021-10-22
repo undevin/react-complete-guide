19341c718cbc1ef7ae4a618bafcaa31939475288
undevin <30535541+undevin@users.noreply.github.com>
2021-10-21 21:50:29 -0500

    Working on it. Bug with the HeaderCartButton

--- food-order-app/src/components/Layout/HeaderCartButton.js
+++ food-order-app/src/components/Layout/HeaderCartButton.js
@@ -1,15 +1,22 @@
-import { Fragment } from 'react';
+import { useContext } from 'react';
+import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItems = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
   return (
-    <button className={styles.button}>
+    <button className={styles.button} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfItems}</span>
     </button>
   );
 };
--- food-order-app/src/components/Meals/MealItem/MealItemForm.js
+++ food-order-app/src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,33 @@
+import { useRef, useState } from 'react';
 import Input from '../../UI/Input';
 import styles from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+  const [amountValid, setAmountValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNum = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNum < 1 ||
+      enteredAmountNum > 5
+    ) {
+      setAmountValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNum);
+  };
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label='Amount'
         input={{
           id: 'amount_' + props.id,
@@ -16,6 +39,7 @@ const MealItemForm = (props) => {
         }}
       />
       <button>+ Add</button>
+      {!amountValid && <p>Please enter a valid amount.</p>}
     </form>
   );
 };
